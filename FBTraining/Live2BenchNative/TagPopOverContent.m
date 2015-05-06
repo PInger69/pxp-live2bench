09ceb2f13124e31ee919a1b55338855bee511675
coop1 <s3aryal@uwaterloo.ca>
2015-05-06 09:33:12 -0400

    catherine - done localization

--- FBTraining/Live2BenchNative/TagPopOverContent.m
+++ FBTraining/Live2BenchNative/TagPopOverContent.m
@@ -72,15 +72,15 @@ typedef enum : NSUInteger {
         contentText = [[NSMutableAttributedString alloc]init];
         // Create the attributed string (text + attributes)
 
-        [self addText: @"Event Date:\t"     style:Bold];          [self addText: eventDate style:Normal];
-        [self addText: @"\nEvent Time:\t"   style:Bold];          [self addText: eventTime style:Normal];
-        [self addText: @"\nHome Team:\t"    style:Bold];          [self addText: homeTeam style:Normal];
-        [self addText: @"\nVisit Team:\t"   style:Bold];          [self addText: visitTeam style:Normal];
+        [self addText: [NSString stringWithFormat:@"%@:\t", NSLocalizedString(@"Event Date", nil)]     style:Bold];          [self addText: eventDate style:Normal];
+        [self addText: [NSString stringWithFormat:@"\n%@:\t", NSLocalizedString(@"Event Time", nil)]   style:Bold];          [self addText: eventTime style:Normal];
+        [self addText: [NSString stringWithFormat:@"\n%@:\t", NSLocalizedString(@"Home Team", nil)]    style:Bold];          [self addText: homeTeam style:Normal];
+        [self addText: [NSString stringWithFormat:@"\n%@:\t", NSLocalizedString(@"Visit Team", nil)]   style:Bold];          [self addText: visitTeam style:Normal];
         if(![leagueName isEqualToString:@""]){
-            [self addText: @"\nLeague:\t"       style:Bold];          [self addText: leagueName style:Normal];
+            [self addText: [NSString stringWithFormat:@"\n%@:\t", NSLocalizedString(@"League", nil)]       style:Bold];          [self addText: leagueName style:Normal];
         }
-        [self addText: @"\nTag Name:\t"     style:Bold];          [self addText: [data objectForKey:@"name"] style:Normal];
-        [self addText: @"\nTag Time:\t\t"     style:Bold];          [self addText: [data objectForKey:@"displaytime"] style:Normal];
+        [self addText: [NSString stringWithFormat:@"\n%@:\t", NSLocalizedString(@"Tag Name", nil)]     style:Bold];          [self addText: [data objectForKey:@"name"] style:Normal];
+        [self addText: [NSString stringWithFormat:@"\n%@:\t\t", NSLocalizedString(@"Tag Time", nil)]   style:Bold];          [self addText: [data objectForKey:@"displaytime"] style:Normal];
         
         
         
