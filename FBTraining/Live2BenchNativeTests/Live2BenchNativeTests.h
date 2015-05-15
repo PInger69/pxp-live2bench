98c308804cea9bfbcfb87adf02a3191778e82972
ncvitak <ncvitak@gmail.com>
2015-05-15 15:52:18 -0400

    Nicholas Cvitak: 	- fixed almost all warnings, only one left :) 	- testing converted to XCTests

--- FBTraining/Live2BenchNativeTests/Live2BenchNativeTests.h
+++ FBTraining/Live2BenchNativeTests/Live2BenchNativeTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 DEV. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface Live2BenchNativeTests : SenTestCase
+@interface Live2BenchNativeTests : XCTestCase
 
 @end
