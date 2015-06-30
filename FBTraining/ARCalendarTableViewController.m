d7556296367bd59b370713128f63efd5ed47dcba
richard <site@avocatec.org>
2015-06-30 17:41:15 -0400

    Richard -Pulling

--- FBTraining/ARCalendarTableViewController.m
+++ FBTraining/ARCalendarTableViewController.m
@@ -255,12 +255,13 @@
                 __block Event * weakEvent = event;
                 if ([localCounterpart.downloadedSources containsObject:[data lastPathComponent]] || [event.downloadedSources containsObject:[data lastPathComponent]]) {
                     
-                    source = [[Feed alloc] initWithFileURL:path];
-                    [localCounterpart setOnComplete:^{
+//                    source = [[Feed alloc] initWithFileURL:path];
+                    source = [localCounterpart.feeds allValues][0];
+                   // [localCounterpart setOnComplete:^{
                         [[NSNotificationCenter defaultCenter] postNotificationName:NOTIF_COMMAND_VIDEO_PLAYER object:nil userInfo:@{@"feed":source, @"command":[NSNumber numberWithInt:VideoPlayerCommandPlayFeed], @"context":STRING_LIVE2BENCH_CONTEXT}];
                         [[NSNotificationCenter defaultCenter] postNotificationName: NOTIF_SELECT_TAB          object:weakSelf userInfo:@{@"tabName":@"Live2Bench"}];
                         
-                    }];
+                  //  }];
                     [weakSelf.encoderManager declareCurrentEvent:localCounterpart];
 
                 } else {
