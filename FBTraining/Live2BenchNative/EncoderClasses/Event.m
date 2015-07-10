abbd6d4ea55d55ed6a86357d18affe3767d83ebb
coop1 <s3aryal@uwaterloo.ca>
2015-07-10 17:09:13 -0400

    catherine - tyring to push

--- FBTraining/Live2BenchNative/EncoderClasses/Event.m
+++ FBTraining/Live2BenchNative/EncoderClasses/Event.m
@@ -223,7 +223,7 @@
         tempDict = [aDict objectForKey:@"mp4_2"];
     } else if ([aDict objectForKey:@"mp4"]) {
         NSDictionary *dic = @{@"hq":[aDict objectForKey:@"mp4"]};
-        [tempDict setObject:dic forKey:@"s_00"];
+        [tempDict setObject:dic forKey:@"onlySource"];
     }
     return [tempDict copy];
 }
@@ -272,11 +272,7 @@
             theFeed =  [[Feed alloc]initWithURLString:aDict[@"live"] quality:0];
             _live = YES;
         } else if (aDict[@"vid"] || aDict[@"mp4"]) {
-            if (isLocal) {
-                theFeed = (isLocal)? [[Feed alloc]initWithFileURL:aDict[@"mp4"][@"hq"]] :  [[Feed alloc]initWithURLString:aDict[@"vid"]  quality:0]  ;
-            }else{
-                theFeed = (isLocal)? [[Feed alloc]initWithFileURL:aDict[@"mp4"][@"hq"]] :  [[Feed alloc]initWithURLString:aDict[@"vid"]  quality:0]  ;
-            }
+            theFeed = (isLocal)? [[Feed alloc]initWithFileURL:aDict[@"mp4"]] :  [[Feed alloc]initWithURLString:aDict[@"vid"]  quality:0]  ;
         } else {
 //            PXPLog(@"Event Warning: No Feeds on Encoder for Event");
 //            PXPLog(@"   HID: %@",aDict[@"hid"]);
