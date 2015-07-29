2fcfbe7438a02c4641bfd762974c20c392f6eab8
ncvitak <ncvitak@gmail.com>
2015-07-29 12:04:32 -0400

    Nicholas Cvitak: 	- fixed local event crash

---
+++ FBTraining/Live2BenchNative/PxpFullscreenButton.h
@@ -0,0 +1,13 @@
+//
+//  PxpFullscreenButton.h
+//  Live2BenchNative
+//
+//  Created by Nico Cvitak on 2015-07-29.
+//  Copyright © 2015 DEV. All rights reserved.
+//
+
+#import "PxpGlowButton.h"
+
+@interface PxpFullscreenButton : PxpGlowButton
+
+@end
---
+++ FBTraining/Live2BenchNative/PxpFullscreenButton.m
@@ -0,0 +1,21 @@
+//
+//  PxpFullscreenButton.m
+//  Live2BenchNative
+//
+//  Created by Nico Cvitak on 2015-07-29.
+//  Copyright © 2015 DEV. All rights reserved.
+//
+
+#import "PxpFullscreenButton.h"
+
+@implementation PxpFullscreenButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
